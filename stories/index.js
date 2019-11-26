0826b1f2cc37c556159ce63d9c6711fda4a3205f
hildakh <hilda.kh.amiri@gmail.com>
2019-11-26 15:54:23 -0800

    Appointment component updated to include the article tag and show the show component if student name and interviewer are added and selected

--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -200,4 +200,20 @@ storiesOf("DayListItem", module) //Initiates Storybook and registers our DayList
           onCancel={action("onCancel")}
           />
         ))
+        .add("Appointment Empty", () => (
+          <Fragment>
+            <Appointment id={1} time="12pm" />
+            <Appointment id="last" time="1pm" />
+          </Fragment>
+        ))
+        .add("Appointment Booked", () => (
+          <Fragment>
+            <Appointment
+              id={1}
+              time="12pm"
+              interview={{ student: "Lydia Miller-Jones", interviewer }}
+            />
+            <Appointment id="last" time="1pm" />
+          </Fragment>
+        ));
 
