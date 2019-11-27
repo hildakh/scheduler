608a8f7db7b95a782239311e02648d7349d44e16
hildakh <hilda.kh.amiri@gmail.com>
2019-11-26 17:09:18 -0800

    Looping over appointments added and the static schedule page is functioning now

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import "components/Application.scss";
 import DayList from "components/DayList";
+import Appointment from "components/Appointment";
 
 const days = [
   {
@@ -21,11 +22,25 @@ const days = [
 ];
 
 const appointments = [
+  
+  {
+    id: 4,
+    time: "9am",
+    interview: {
+      student: "Ross Geller",
+      interviewer: {
+        id: 2,
+        name: "Chandler Bing",
+        avatar: "https://i.imgur.com/FK8V841.jpg" 
+      }
+    }
+  },
+
   {
     id: 1,
     time: "12pm"
   },
-  
+
   {
     id: 2,
     time: "1pm",
@@ -52,18 +67,7 @@ const appointments = [
     }
   },
 
-  {
-    id: 4,
-    time: "9am",
-    interview: {
-      student: "Ross Geller",
-      interviewer: {
-        id: 2,
-        name: "Chandler Bing",
-        avatar: "https://i.imgur.com/FK8V841.jpg" 
-      }
-    }
-  }
+  
 ];
 
 export default function Application(props) {
@@ -92,7 +96,16 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointments.map(appointment => {
+          return (
+            <Appointment
+            key={appointment.id}
+            {...appointment}
+            />
+          )
+        })
+        }
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
