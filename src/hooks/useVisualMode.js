24fee8f1ce14fd1d39edbf38845dbc906c1ee788
hildakh <hilda.kh.amiri@gmail.com>
2019-11-28 12:13:04 -0800

    replace argument added to the transition function to pass the test

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,10 +4,19 @@ export default function useVisualMode(initial) {
   const [history, setHistory] = useState([initial]);
   const [mode, setMode] = useState(initial);
   
-  function transition(mode) {
+  function transition(mode, replace) {
+    //setMode to the current mode
     setMode(mode);
+    //If there is need for replace
+    if(replace) {
+      //empty the history array and add the mode to it
+      setHistory([...history.slice(0, history.length -1), mode]);
+    } else {
+      //add the current mode to the history array
+      //set the history
     history.push(mode)
     setHistory(history);
+    }
   }
 
   function back() {
