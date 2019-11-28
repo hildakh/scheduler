e0be6d2f49509b60c71a3acfc1a15a4d8baf09e2
hildakh <hilda.kh.amiri@gmail.com>
2019-11-27 20:43:10 -0800

    custom hooks added to pass the tests for the first and second constant.

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  function transition(mode) {
+    setMode(mode);
+  }
+  return { mode,  transition };
+}
