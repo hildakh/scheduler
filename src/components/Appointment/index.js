5d676044be8fe31a4a2f7dd0bc8552ac942adfb3
hildakh <hilda.kh.amiri@gmail.com>
2019-11-28 21:04:44 -0800

    async status and saving indicator added

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,8 +5,18 @@ import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
 import useVisualMode from "hooks/useVisualMode"
 import Form from "./Form";
+import Status from "./Status";
 
 export default function Appointment(props) {
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
+const SAVING = "SAVING"
+
+const { mode, transition, back } = useVisualMode(
+  props.interview ? SHOW : EMPTY
+)
 
 function save(name, interviewer) {
   const interview = {
@@ -14,19 +24,13 @@ function save(name, interviewer) {
     interviewer
   };
 
+  transition(SAVING)
   props.bookInterview(props.id, interview)
   .then(() =>  {
      transition(SHOW)
      })
-  
 }
-const EMPTY = "EMPTY";
-const SHOW = "SHOW";
-const CREATE = "CREATE";
 
-const { mode, transition, back } = useVisualMode(
-  props.interview ? SHOW : EMPTY
-)
   return (
   <article className="appointment">
   <Header time={props.time}/>
@@ -41,6 +45,9 @@ const { mode, transition, back } = useVisualMode(
   )}
   {mode === CREATE && 
   <Form onCancel={back} onSave={save} interviewers={props.interviewers}/>
+  }
+  {mode === SAVING && 
+  <Status message="Saving"/>
   }
   </article>
   );
