f10222556995028d35868521ddefeca414f25025
hildakh <hilda.kh.amiri@gmail.com>
2019-12-02 18:14:39 -0800

    the first test modified for application and successfully passed. Our fake data contains a record for a day with the text Monday. Before we request the data from the API, we won't render any days. When we pass the data contained in the response through the component tree, the first list item will render the text Monday. We can instruct our test to wait for the element with the text Monday.

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -3,11 +3,17 @@ import React from "react";
 import { render, cleanup, waitForElement } from "@testing-library/react";
 
 import Application from "components/Application";
+import { fireEvent } from "@testing-library/react/dist";
+import { exportAllDeclaration } from "@babel/types";
 
 afterEach(cleanup);
 
 it("defaults to Monday and changes the schedule when a new day is selected", () => {
   const { getByText } = render(<Application />);
 
-  return waitForElement(() => getByText("Monday"));
+  return waitForElement(() => getByText("Monday"))
+  .then(() => { 
+    fireEvent.click(getByText("Tuesday"));
+    expect(getByText("Leopold Silvers")).toBeInTheDocument();
+});
 });
