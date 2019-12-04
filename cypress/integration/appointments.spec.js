d8be0746854cf574a6919e0aa2ef891a6de17a1e
hildakh <hilda.kh.amiri@gmail.com>
2019-12-03 19:31:57 -0800

    refactored the tests

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,15 +1,20 @@
 describe("Appointments", () => {
-  it("should book an interview", () => {
-    cy.request("GET", "/api/debug/reset")
+  beforeEach( ()=> {
+    cy.request("GET", "/api/debug/reset");
+
     cy.visit("/");
+
     cy.contains("Monday");
-    
+  });
+
+  it("should book an interview", () => {    
     cy.get("[alt=Add]")
       .first()
       .click();
+
     cy.get("[data-testid=student-name-input]").type("Hilda Matilda");
-    
     cy.get("[alt='Sylvia Palmer']").click();
+    
     cy.contains("Save").click();
 
     cy.contains(".appointment__card--show", "Hilda Matilda");
