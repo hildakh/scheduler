bbd1cfd22438a43ee1e194287b82576f4f81dfb8
hildakh <hilda.kh.amiri@gmail.com>
2019-12-03 16:32:31 -0800

    added the       src/**/*.js,

--- src/reducers/application.js
+++ src/reducers/application.js
@@ -1,4 +1,3 @@
-import { useReducer, useEffect } from "react";
 export const SET_DAY = "SET_DAY";
 export const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
 export const SET_INTERVIEW = "SET_INTERVIEW";
