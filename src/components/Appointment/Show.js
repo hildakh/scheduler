84e43775a2186f8e30f87822a2febb9b0b1ae4e4
hildakh <hilda.kh.amiri@gmail.com>
2019-12-09 19:58:59 -0800

    changed the way the interviewer name is accessed and shown on the Show component

--- src/components/Appointment/Show.js
+++ src/components/Appointment/Show.js
@@ -6,10 +6,10 @@ export default function Show(props) {
       <section className="appointment__card-left">
         <h2 className="text--regular">{props.student}</h2>
         <section className="interviewer">
-          <h4 className="text--light">{props.interviewer}</h4>
-          <h3 className="text--regular">{props.interviewer}</h3>
+          <h4 className="text--light">Interviewer</h4>
+          <h3 className="text--regular">{props.interviewer.name}</h3>
         </section>
-      </section>
+      </section>  
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <img
