42746bd6290244be7cb1659c2caa99e7e1c68338
Hilda <hilda.kh.amiri@gmail.com>
2020-11-08 18:42:20 -0800

    updating setMode and setHistory to not mutate state

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,29 +1,32 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
-export default function useVisualMode(initial) {
+const useVisualMode = (initial) => {
   const [history, setHistory] = useState([initial]);
   const [mode, setMode] = useState(initial);
-  
-  function transition(mode, replace) {
+
+  const transition = (mode, replace) => {
     //setMode to the current mode
     setMode(mode);
     //If there is need for replace
-    if(replace) {
-      //empty the history array and add the mode to it
-      setHistory([...history.slice(0, history.length -1), mode]);
+    if (replace) {
+      //remove the last item in the array to replace it with the new mode
+      setHistory((history) => [...history.slice(0, history.length - 1), mode]);
     } else {
       //add the current mode to the history array
       //set the history
-    history.push(mode)
-    setHistory(history);
+      setHistory((history) => [...history, mode]);
     }
-  }
+  };
 
   function back() {
-    if(history.length > 1) {
-    history.pop();
-    setMode(history[history.length-1]);
+    if (history.length > 1) {
+      setMode(history[history.length - 2]);
+      setHistory((history) => [...history.slice(0, history.length - 1)]);
+    } else {
+      setMode(history[history.length - 1]);
+    }
   }
-}
-  return { mode,  transition, back };
-}
+  return { mode, transition, back };
+};
+
+export default useVisualMode;
