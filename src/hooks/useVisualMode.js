018edb34b0fe4c9add066586c3170822243eaf84
Hilda <hilda.kh.amiri@gmail.com>
2020-11-08 16:59:50 -0800

    dependency update and state update to not mutate state

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -10,17 +10,17 @@ const useVisualMode = (initial) => {
     //If there is need for replace
     if (replace) {
       //remove the last item in the array to replace it with the new mode
-      setHistory((history) => [...history.slice(0, history.length - 1), mode]);
+      setHistory((prev) => [...prev.slice(0, prev.length - 1), mode]);
     } else {
       //add the current mode to the history array
       //set the history
-      setHistory((history) => [...history, mode]);
+      setHistory((prev) => [...prev, mode]);
     }
   };
 
   function back() {
     if (history.length > 1) {
-      setHistory((history) => [...history.slice(0, history.length - 1)]);
+      setHistory((prev) => [...prev.slice(0, prev.length - 1)]);
     }
   }
   return { mode, transition, back };
